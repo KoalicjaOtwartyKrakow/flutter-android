cd638b8af7cd2646fc71dec94dd59c9c881e48bf
magdalena-b <magdexxa@gmail.com>
2022-03-12 20:37:04 +0100

    Update guest form and home page

--- lib/presentation/guest/widgets/guest_form.dart
+++ lib/presentation/guest/widgets/guest_form.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:i18next/i18next.dart';
