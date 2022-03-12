89c56d89c401a496bfc0febe1157f00a11123f37
magdalena-b <magdexxa@gmail.com>
2022-03-12 20:35:08 +0100

    Add first version of guest form event

--- lib/presentation/guest/guest_form_bloc/guest_form_event.dart
+++ lib/presentation/guest/guest_form_bloc/guest_form_event.dart
@@ -3,4 +3,12 @@ part of 'guest_form_bloc.dart';
 @freezed
 class GuestFormEvent with _$GuestFormEvent {
   const factory GuestFormEvent.submitAddGuest(GuestDto guest) = SubmitAddGuest;
+abstract class GuestFormEvent with _$GuestFormEvent {
+
+  const factory GuestFormEvent.fullNameChanged(String value) = FullNameChanged;
+  const factory GuestFormEvent.phoneNumberChanged(String value) = PhoneNumberChanged;
+  const factory GuestFormEvent.emailChanged(String value) = EmailChanged;
+
+  const factory GuestFormEvent.submitAddGuest() = SubmitAddGuest;
+
 }
