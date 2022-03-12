f7043587513202b6cdef8ff28e1b7db1d344fc97
magdalena-b <magdexxa@gmail.com>
2022-03-12 20:35:32 +0100

    Add first version of guest form state

--- lib/presentation/guest/guest_form_bloc/guest_form_state.dart
+++ lib/presentation/guest/guest_form_bloc/guest_form_state.dart
@@ -6,4 +6,23 @@ class GuestFormState with _$GuestFormState {
   const factory GuestFormState.initial() = _Initial;
   const factory GuestFormState.submitSuccess() = _SubmitSuccess;
   const factory GuestFormState.submitFailure(Failure failure) = _SubmitFailure;
+abstract class GuestFormState with _$GuestFormState {
+  const factory GuestFormState({
+    required String fullName,
+    required String email,
+    required String phoneNumber,
+    // required AutovalidateMode showErrorMessages,
+    required bool showErrorMessages,
+    required bool isSubmitting,
+    required Option<Either<SubmitFailure, bool>> submitFailureOrSuccessOption,
+  }) = _GuestFormState;
+
+  factory GuestFormState.initial() => GuestFormState(
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    showErrorMessages: false,
+    isSubmitting: false,
+    submitFailureOrSuccessOption: none(),
+  );
 }
