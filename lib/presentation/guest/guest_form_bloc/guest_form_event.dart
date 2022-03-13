210202a53a939914fc79bde855f62f3dc1d5a733
magdalena-b <magdexxa@gmail.com>
2022-03-13 15:23:02 +0100

    Small change in guest form event

--- lib/presentation/guest/guest_form_bloc/guest_form_event.dart
+++ lib/presentation/guest/guest_form_bloc/guest_form_event.dart
@@ -5,9 +5,10 @@ class GuestFormEvent with _$GuestFormEvent {
   const factory GuestFormEvent.submitAddGuest(GuestDto guest) = SubmitAddGuest;
 abstract class GuestFormEvent with _$GuestFormEvent {
 
-  const factory GuestFormEvent.fullNameChanged(String value) = FullNameChanged;
-  const factory GuestFormEvent.phoneNumberChanged(String value) = PhoneNumberChanged;
-  const factory GuestFormEvent.emailChanged(String value) = EmailChanged;
+
+  const factory GuestFormEvent.fullNameChanged(String fullNameStr) = FullNameChanged;
+  const factory GuestFormEvent.phoneNumberChanged(String phoneNumberStr) = PhoneNumberChanged;
+  const factory GuestFormEvent.emailChanged(String emailStr) = EmailChanged;
 
   const factory GuestFormEvent.submitAddGuest() = SubmitAddGuest;
 
