8ea6b3feb685ceb70418487d488aab8c9149f39f
magdalena-b <magdexxa@gmail.com>
2022-03-13 15:22:39 +0100

    Add first version of guest form bloc

--- lib/presentation/guest/guest_form_bloc/guest_form_bloc.dart
+++ lib/presentation/guest/guest_form_bloc/guest_form_bloc.dart
@@ -5,6 +5,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../../infrastructure/api_client.dart';
+import '../../../models/guest.dart';
 import '../submit_failure.dart';
 
 part 'guest_form_event.dart';
@@ -17,11 +19,56 @@ class GuestFormBloc extends Bloc<GuestFormEvent, GuestFormState> {
   @override
   GuestFormState get initialState => GuestFormState.initial();
 
+  final ApiClient apiClient;
 
+  GuestFormBloc(this.apiClient) : super(GuestFormState.initial()) {
+    on<FullNameChanged>(
+          (event, emit) {
+        emit(
+          state.copyWith(
+            fullName: event.fullNameStr,
+            submitFailureOrSuccessOption: none(),
+          ),
+        );
+      },
+    );
+    on<PhoneNumberChanged>(
+          (event, emit) {
+        emit(
+          state.copyWith(
+            fullName: event.phoneNumberStr,
+            submitFailureOrSuccessOption: none(),
+          ),
+        );
+      },
+    );
+    on<EmailChanged>(
+          (event, emit) {
+        emit(
+          state.copyWith(
+            email: event.emailStr,
+            submitFailureOrSuccessOption: none(),
+          ),
+        );
+      },
+    );
+    on<SubmitAddGuest>(
+          (event, emit) async {
+        emit(state.copyWith(
+          isSubmitting: true,
+          submitFailureOrSuccessOption: none(),
+        ));
 
-  GuestFormBloc() : super(GuestFormState.initial()) {
-    on<FullNameChanged>((event, emit) {
-      // TODO: implement event handler
-    });
+        final failureOrSuccess = await apiClient.postAGuest(Guest(
+          fullName: state.fullName,
+          phoneNumber: state.phoneNumber,
+          email: state.email
+        ));
+        emit(state.copyWith(
+          isSubmitting: false,
+          submitFailureOrSuccessOption: some(failureOrSuccess),
+        ));
+      },
+    );
   }
 }
