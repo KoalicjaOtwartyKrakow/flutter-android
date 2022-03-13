e28d30128b77610a09f96483c6c65efafd880f55
magdalena-b <magdexxa@gmail.com>
2022-03-13 15:23:02 +0100

    Small change in guest form event

--- lib/presentation/guest/guest_form_bloc/guest_form_event.dart
+++ lib/presentation/guest/guest_form_bloc/guest_form_event.dart
@@ -3,9 +3,10 @@ part of 'guest_form_bloc.dart';
 @freezed
 abstract class GuestFormEvent with _$GuestFormEvent {
 
-  const factory GuestFormEvent.fullNameChanged(String value) = FullNameChanged;
-  const factory GuestFormEvent.phoneNumberChanged(String value) = PhoneNumberChanged;
-  const factory GuestFormEvent.emailChanged(String value) = EmailChanged;
+
+  const factory GuestFormEvent.fullNameChanged(String fullNameStr) = FullNameChanged;
+  const factory GuestFormEvent.phoneNumberChanged(String phoneNumberStr) = PhoneNumberChanged;
+  const factory GuestFormEvent.emailChanged(String emailStr) = EmailChanged;
 
   const factory GuestFormEvent.submitAddGuest() = SubmitAddGuest;
 
