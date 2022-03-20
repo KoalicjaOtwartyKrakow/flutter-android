259ef30507f64bd5ef63f9f04c5b228ec25b7cce
magdalena-b <magdexxa@gmail.com>
2022-03-20 17:20:09 +0100

    Add proper toJson and fromJson methods in ChildAge

--- lib/infrastructure/converters.dart
+++ lib/infrastructure/converters.dart
@@ -17,33 +17,3 @@ String? nullableDateTimeToString(DateTime? dateTime) {
   }
   return null;
 }
-
-List<ChildAge> childrenAgesFromString(String childrenAgesStr) {
-  List<ChildAge> childrenAges = [];
-  childrenAgesStr.replaceAll("[", '');
-  childrenAgesStr.replaceAll("]", '');
-  var splitAges = childrenAgesStr.split(',');
-  for (String ageStr in splitAges) {
-    ChildAge childAge = ChildAge();
-    childAge.age = int.parse(ageStr);
-    childrenAges.add(childAge);
-  }
-  return childrenAges;
-}
-
-String childrenAgesToString(List<ChildAge> childrenAges) {
-  String childrenAgesStr = '[';
-  for (ChildAge age in childrenAges) {
-    childrenAgesStr += age.toString() + ",";
-  }
-  childrenAgesStr += "]";
-  return childrenAgesStr;
-}
-
-
-String? nullableChildenAgesToString(List<ChildAge>? childrenAges) {
-  if (childrenAges != null) {
-    return childrenAgesToString(childrenAges);
-  }
-  return null;
-}
---
+++ lib/models/child_age.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'child_age.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChildAge _$ChildAgeFromJson(Map<String, dynamic> json) => ChildAge(
+      age: json['age'] as int?,
+    );
+
+Map<String, dynamic> _$ChildAgeToJson(ChildAge instance) => <String, dynamic>{
+      'age': instance.age,
+    };
