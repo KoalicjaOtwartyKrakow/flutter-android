dd25495ac17496b4ccf9ed0a235f8ee524657470
magdalena-b <magdexxa@gmail.com>
2022-04-03 22:57:32 +0200

    Add i18n for guest page and guest form, delete IGuestRepository, add postGuest to apiClient

--- lib/infrastructure/guest/i_guest_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:injectable/injectable.dart';
-
-import '../../models/guest.dart';
-import '../../models/failure.dart';
-
-abstract class IGuestRepository {
-  Future<Either<Failure, Guest>> postGuest(Guest guest);
-}
